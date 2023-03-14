8c0fac14f2cb53ea5d80d1d26c19b32fc76b2f19
jessicaHyp <127483032+jessicaHyp@users.noreply.github.com>
2023-03-14 15:50:32 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,80 @@
+if(typeof window !== "undefined"){
+  window.addEventListener('DOMContentLoaded', function() {
+  // Part 1: Ensure that the user can play the game.
+  var cards = document.querySelectorAll('.card');
+  var selectedCards = [];
+  var matchedCards = [];
+
+  cards.forEach(function(card) {
+    card.addEventListener('click', function() {
+      // If the card has already been matched, ignore it.
+      if (card.classList.contains('is-matched')) {
+        return;
+      }
+
+      // If we haven't selected 2 cards yet, add the current card to the
+      // collection of selected cards and apply the correct CSS class.
+      if (selectedCards.length < 3) {
+        card.classList.add('is-selected');
+        selectedCards.push(card);
+        console.log(selectedCards);
+        console.log(card.classList)
+      }
+
+      // If we have selected two cards, see if they match.
+      if (selectedCards.length === 2) {
+        var card1 = selectedCards[0];
+        var card2 = selectedCards[1];
+        console.log(card1)
+        console.log(card2)
+
+        // If the cards match, add them to the collection of matched cards and
+        // apply the correct CSS class.
+        if (card1.innerText === card2.innerText) {
+          matchedCards.push(card1, card2);
+          card1.classList.add('is-matched');
+          card2.classList.add('is-matched');
+    
+        }
+
+        // Regardless of whether or not the cards match, deselect them and reset
+        // the collection of matched cards.
+        card1.classList.remove('is-selected');
+        card2.classList.remove('is-selected');
+        selectedCards = [];
+      }
+
+      // If we've matched all the cards, display a message.
+      if (matchedCards.length === cards.length) {
+        alert('You matched all the cards, nice job!');
+      }
+    });
+  });
+
+
+  // Part 2: Allow the user to customize the colour of the cards.
+  // This is achieved by including one of the following values in the URL:
+  // - #green
+  // - #orange
+  // - #red
+  var deckElement = document.querySelector('.cards');
+  console.log(deckElement.classList)
+  console.log(deckElement.querySelector('.child'))
+  
+  console.log(deckElement.className)
+  var deckColorHash = window.location.hash;
+  console.log(deckColorHash)
+
+
+  if (deckColorHash && deckElement) {
+    let deckColor = deckColorHash.replace("#","")
+    var className = '--'+deckColor;
+   
+  
+   
+    deckElement.classList.add(className);
+   
+    
+    console.log(deckElement.className)
+  }
+})};
